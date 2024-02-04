d525ba3e7b5fe5cfec9c4de9e57e210b10b4e815
WalterHenri <walterdoideira5438@gmail.com>
2024-02-04 14:53:06 -0300

    enhacements

--- Enhancement.py
+++ Enhancement.py
@@ -6,7 +6,6 @@ import abc
 class Enhancement(Object, metaclass=abc.ABCMeta):
     def __init__(self, x, y, z):
         super().__init__(x, y, z)
-        self.scale = 1
         self.is_animated = False
 
     @abc.abstractmethod
@@ -25,3 +24,12 @@ class Cactus(Enhancement):
         screen.blit(scaled_sprite, (x, y))
 
 
+class Tree(Enhancement):
+    def __init__(self, x, y, z):
+        super().__init__(x, y, z)
+        self.sprite = pygame.image.load("Assets/")
+
+    def draw(self, screen, x, y, scale):
+        scaled_sprite = pygame.transform.scale(self.sprite, (
+            int(self.sprite.get_width() * scale), int(self.sprite.get_height() * scale)))
+        screen.blit(scaled_sprite, (x, y))
